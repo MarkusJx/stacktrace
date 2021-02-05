f02f94f3997a25e51b98e4da11237127b16d2c64
Markus <28785953+MarkusJx@users.noreply.github.com>
2021-02-05 12:47:23 +0100

    Update stuff

--- addr2lineLib/addr2line.c
+++ addr2lineLib/addr2line.c
@@ -68,7 +68,7 @@
 
 #define bfd_section_flags(section) section->flags
 
-#ifdef __APPLE__
+#ifndef __APPLE__
 #   define bfd_section_vma(abfd, section) bfd_section_vma(section)
 #   define bfd_section_size(abfd, section) bfd_section_size(section)
 #   define DMGL_PARAMS (1 << 0)
